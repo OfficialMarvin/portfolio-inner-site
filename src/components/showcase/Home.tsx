dcd1ee1119bc29e3fc1b1db66118035c333c6263
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-03-21 19:35:26 -0400

    Update Home.tsx

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from '../general';
-
-import forhire from '../../assets/pictures/forHireGif.gif';
 import { useNavigate } from 'react-router';
 
 export interface HomeProps {}
@@ -16,29 +14,23 @@ const Home: React.FC<HomeProps> = (props) => {
     return (
         <div style={styles.page}>
             <div style={styles.header}>
-                <h1 style={styles.name}>Henry Heffernan</h1>
-                <h2>Software Engineer</h2>
+                <h1 style={styles.name}>Marvin Jakobs</h1>
+                <h2>Data Scientist</h2>
             </div>
             <div style={styles.buttons}>
                 <Link containerStyle={styles.link} to="about" text="ABOUT" />
-                <Link
-                    containerStyle={styles.link}
-                    to="experience"
-                    text="EXPERIENCE"
-                />
-                <Link
-                    containerStyle={styles.link}
-                    to="projects"
-                    text="PROJECTS"
-                />
-                <Link
-                    containerStyle={styles.link}
-                    to="contact"
-                    text="CONTACT"
-                />
+                <Link containerStyle={styles.link} to="experience" text="EXPERIENCE" />
+                <Link containerStyle={styles.link} to="projects" text="PROJECTS" />
+                <Link containerStyle={styles.link} to="contact" text="CONTACT" />
+            </div>
+            <div style={styles.contactInfo}>
+                <p>LinkedIn: <a href="https://linkedin.com/in/marvinjakobs">linkedin.com/in/marvinjakobs</a></p>
+                <p>GitHub: <a href="https://github.com/OfficialMarvin">github.com/OfficialMarvin</a></p>
+                <p>Email: <a href="mailto:mkj5388@psu.edu">mkj5388@psu.edu</a></p>
+                <p>Mobile: +1-612-298-3926</p>
             </div>
             <div style={styles.forHireContainer} onMouseDown={goToContact}>
-                {/* <img src={forhire} alt="" /> */}
+                <p>Available for data science internships and full-time opportunities!</p>
             </div>
         </div>
     );
@@ -59,7 +51,6 @@ const styles: StyleSheetCSS = {
         textAlign: 'center',
         marginBottom: 64,
         marginTop: 64,
-
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
@@ -67,14 +58,7 @@ const styles: StyleSheetCSS = {
     buttons: {
         justifyContent: 'space-between',
     },
-    image: {
-        width: 800,
-    },
     link: {
-        padding: 16,
-    },
-    nowHiring: {
-        backgroundColor: 'red',
         padding: 16,
     },
     forHireContainer: {
@@ -88,6 +72,10 @@ const styles: StyleSheetCSS = {
         fontSize: 72,
         marginBottom: 16,
         lineHeight: 0.9,
+    },
+    contactInfo: {
+        marginTop: 32,
+        textAlign: 'center',
     },
 };
 

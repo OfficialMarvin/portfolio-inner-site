d07ab7de86b363b0352b7bb176f7f2e4f1c91df8
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-03-21 19:37:09 -0400

    Update Projects.tsx

--- src/components/showcase/Projects.tsx
+++ src/components/showcase/Projects.tsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
-import software from '../../assets/pictures/projects/software.gif';
-import art from '../../assets/pictures/projects/art.gif';
-import music from '../../assets/pictures/projects/music.gif';
 
 export interface ProjectsProps {}
 
 interface ProjectBoxProps {
-    icon: string;
     title: string;
-    subtitle: string;
+    description: string;
     route: string;
-    iconStyle: React.CSSProperties;
 }
 
 const ProjectBox: React.FC<ProjectBoxProps> = ({
-    icon,
     title,
-    subtitle,
+    description,
     route,
-    iconStyle,
 }) => {
     const [, setIsHovering] = useState(false);
     const navigation = useNavigate();
@@ -45,18 +38,9 @@ const ProjectBox: React.FC<ProjectBoxProps> = ({
             onMouseLeave={onMouseLeave}
         >
             <div style={styles.projectLinkLeft}>
-                <img
-                    src={icon}
-                    style={Object.assign(
-                        {},
-                        styles.projectLinkImage,
-                        iconStyle
-                    )}
-                    alt=""
-                />
                 <div style={styles.projectText}>
-                    <h1 style={{ fontSize: 48 }}>{title}</h1>
-                    <h3>{subtitle}</h3>
+                    <h1 style={{ fontSize: 32 }}>{title}</h1>
+                    <p>{description}</p>
                 </div>
             </div>
             <div style={styles.projectLinkRight}></div>
@@ -68,36 +52,22 @@ const Projects: React.FC<ProjectsProps> = (props) => {
     return (
         <div className="site-page-content">
             <h1>Projects</h1>
-            <h3>& Hobbies</h3>
-            <br />
-            <p>
-                Click on one of the areas below to check out some of my favorite
-                projects I've done in that field. I spent a lot of time to
-                include a lot of visuals and interactive media to showcase each
-                project. Enjoy!
-            </p>
             <br />
             <div style={styles.projectLinksContainer}>
                 <ProjectBox
-                    icon={software}
-                    iconStyle={styles.computerIcon}
-                    title="Software"
-                    subtitle="PROJECTS"
-                    route="software"
+                    title="Blockchain Battleship"
+                    description="Developed a decentralized game on Ethereum with Solidity and JavaScript. Features included real-time messaging and gameplay mechanics, with a focus on smart contract security and frontend interaction via Web3.js."
+                    route="blockchain-battleship"
                 />
                 <ProjectBox
-                    icon={music}
-                    iconStyle={styles.musicIcon}
-                    title="Music"
-                    subtitle="VENTURES"
-                    route="music"
+                    title="Stock Trend Prediction"
+                    description="Utilized sentiment analysis on tweets and historical stock data to predict stock trends with a model accuracy of 51%. Implemented data processing and machine learning models in Databricks, employing VADER for sentiment analysis."
+                    route="stock-trend-prediction"
                 />
                 <ProjectBox
-                    icon={art}
-                    iconStyle={styles.artIcon}
-                    title="Art"
-                    subtitle="ENDEAVORS"
-                    route="art"
+                    title="Brain Tumor Classification"
+                    description="Developed machine learning models for grading gliomas using imaging and genomic data, emphasizing algorithm selection and model accuracy. Used datasets from UC Irvine and TCGA, tested SVM, Random Forest, and neural networks."
+                    route="brain-tumor-classification"
                 />
             </div>
         </div>
@@ -116,7 +86,6 @@ const styles: StyleSheetCSS = {
         cursor: 'pointer',
         width: '100%',
         boxSizing: 'border-box',
-
         alignItems: 'center',
         justifyContent: 'space-between',
     },
@@ -124,30 +93,9 @@ const styles: StyleSheetCSS = {
         justifyContent: 'center',
         flexDirection: 'column',
     },
-    projectLinkImage: {
-        width: 48,
-        // height: 48,
-        marginRight: 38,
-    },
     projectLinkLeft: {
         marginLeft: 16,
         alignItems: 'center',
-    },
-    computerIcon: {
-        width: 56,
-        height: 56,
-    },
-    musicIcon: {
-        width: 48,
-        height: 48,
-    },
-    arrowIcon: {
-        width: 48,
-        height: 48,
-    },
-    artIcon: {
-        width: 21 * 2,
-        height: 37 * 2,
     },
 };
 

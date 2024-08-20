eb39ebcc8849fbe883b5b1ef270c973bfa5a20dc
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-08-19 20:21:15 -0400

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -114,8 +114,8 @@ const About: React.FC<AboutProps> = (props) => {
                     them. You can reach me through the{' '}
                     <Link to="/contact">contact page</Link> or shoot me an email
                     at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:marv416@gmail.com">
+                        marv416@gmail.com
                     </a>
                 </p>
             </div>

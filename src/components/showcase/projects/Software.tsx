9ea1e90b27c38708f2b95530f31839e04420dbb4
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-09-24 16:49:12 -0400

    Update Software.tsx

--- src/components/showcase/projects/Software.tsx
+++ src/components/showcase/projects/Software.tsx
@@ -26,7 +26,7 @@ const SoftwareProjects: React.FC<SoftwareProjectsProps> = (props) => {
                 </p>
                 <br />
                 <div className="captioned-image">
-                    <src={"https://s1.gifyu.com/images/SAgmE.gif"} />
+                    <img src={"https://s1.gifyu.com/images/SAgmE.gif"} />
                     <p style={styles.caption}>
                         <sub>
                             <b>Figure 1:</b> Visualization of the Ocean Trash Detection system in action.
